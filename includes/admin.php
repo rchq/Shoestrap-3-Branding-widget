f7b51a73e353d20f11955fcab4cb4979f9712dd3
rchq <design@rchq.com>
2014-08-27 14:32:15 -0700

    clean

--- includes/admin.php
+++ includes/admin.php
@@ -1,59 +1,55 @@
 <?php
-
-
 /*
  * Shoestrap 3 Header Widget options
  */
 if ( !function_exists( 'shoestrap_hw_module_options' ) ) {
-	function shoestrap_hw_module_options( $sections ) {
-		$section = array(
-			'title' => __( 'Header Widget', 'shoestrap_hw' ),
-			'icon'  => 'el-icon-check-empty  '
-		);
+ function shoestrap_hw_module_options( $sections ) {
+  $section = array(
+   'title' => __( 'Header Widget', 'shoestrap_hw' ),
+   'icon'  => 'el-icon-check-empty  '
+  );
 			
-			
-		$fields[] = array(
-			"title"     	=> __("Display the Header.", "shoestrap_hw"),
-			"desc"      	=> __("Turn this ON to display the header. Default: OFF", "shoestrap"),
-			"id"       		=> "header_widget_toggle",
-			"customizer"	=> array(),
-			"std"       	=> 0,
-			"type"      	=> "switch"
-		);
-		$fields[] = array(
-			'title'				=> __( 'Header Background Color', 'shoestrap_hw' ),
-			'desc'      	=> __( 'Select the background color for your header. Default: #EEEEEE.', 'shoestrap_hw' ),
-			'id'        	=> 'header_widget_bg',
-			'default'     => '#eeeeee',
-			'compiler'    => true,
-			'transparent' => false,
-			'type'        => 'color'
-		);
-	
-		$fields[] = array(
-			'title'				=> __('Header Text Color', 'shoestrap_hw'),
-			'desc'      	=> __('Select the text color for your header. Default: #333333.', 'shoestrap_hw'),
-			'id'        	=> 'header_widget_color',
-			'default'     => '#333333',
-			'compiler'    => true,
-			'transparent' => false,
-			'type'        => 'color'
-		);
-			
-		$fields[] = array(
-			'title'				=> __('Header logo URL', 'shoestrap_hw'),
-			'desc'      	=> __('Set a custom URL', 'shoestrap_hw'),
-			'id'        	=> 'header_widget_url',
-			'validate'  	=> 'url',
-			'type'      	=> 'text',
-		);
-
-		$section['fields'] = $fields;
-	
-		$section = apply_filters( 'shoestrap_hw_module_options_modifier', $section );
+  $fields[] = array(
+   "title"      => __("Display the Header.", "shoestrap_hw"),
+   "desc"       => __("Turn this ON to display the header. Default: OFF", "shoestrap_hw"),
+   "id"         => "header_widget_toggle",
+   "customizer" => array(),
+   "std"        => 0,
+   "type"       => "switch"
+  );
 		
-		$sections[] = $section;
-		return $sections;
-	}
+  $fields[] = array(
+   'title'       => __( 'Header Background Color', 'shoestrap_hw' ),
+   'desc'        => __( 'Select the background color for your header. Default: #EEEEEE.', 'shoestrap_hw' ),
+   'id'          => 'header_widget_bg',
+   'default'     => '#eeeeee',
+   'compiler'    => true,
+   'transparent' => true,
+   'type'        => 'color'
+  );
+	
+  $fields[] = array(
+   'title'       => __('Header Text Color', 'shoestrap_hw'),
+   'desc'        => __('Select the text color for your header. Default: #333333.', 'shoestrap_hw'),
+   'id'          => 'header_widget_color',
+   'default'     => '#333333',
+   'compiler'    => true,
+   'transparent' => false,
+   'type'        => 'color'
+  );
+			
+  $fields[] = array(
+   'title'      => __('Header logo URL', 'shoestrap_hw'),
+   'desc'       => __('Set a custom URL', 'shoestrap_hw'),
+   'id'         => 'header_widget_url',
+   'validate'   => 'url',
+   'type'       => 'text',
+  );
+
+  $section['fields'] = $fields;
+  $section = apply_filters( 'shoestrap_hw_module_options_modifier', $section );
+  $sections[] = $section;
+  return $sections;
+ }
 }
 add_filter( 'redux/options/' . SHOESTRAP_OPT_NAME . '/sections', 'shoestrap_hw_module_options', 17 );
--- plugin.php
+++ plugin.php
@@ -9,22 +9,22 @@ Author URI:  http://rchq.com
 */
 
 if ( !defined( 'REDUX_OPT_NAME' ) )
-	define( 'REDUX_OPT_NAME', 'shoestrap' );
+ define( 'REDUX_OPT_NAME', 'shoestrap' );
 
 // plugin folder url
 if ( !defined( 'S3HW_PLUGIN_URL' ) )
-	define( 'S3HW_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+ define( 'S3HW_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
 // plugin folder path
 if ( !defined( 'S3HW_PLUGIN_DIR' ) )
-	define( 'S3HW_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+ define( 'S3HW_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
 // plugin root file
 if ( !defined( 'S3HW_PLUGIN_FILE' ) )
-	define( 'S3HW_PLUGIN_FILE', __FILE__ );
+ define( 'S3HW_PLUGIN_FILE', __FILE__ );
 
 function shoestrap_hw_include_files() {
-	include_once( S3HW_PLUGIN_DIR . 'includes/admin.php' );
-	include_once( S3HW_PLUGIN_DIR . 'includes/functions.php' );
+ include_once( S3HW_PLUGIN_DIR . 'includes/admin.php' );
+ include_once( S3HW_PLUGIN_DIR . 'includes/functions.php' );
 }
 add_action( 'shoestrap_include_files', 'shoestrap_hw_include_files' );
