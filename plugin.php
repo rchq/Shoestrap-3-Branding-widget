d83be2b68459051caee6019a3d1c7edd4e119020
rchq <design@rchq.com>
2014-09-04 16:22:39 -0700

    tweaks

--- plugin.php
+++ plugin.php
@@ -13,22 +13,22 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) { 
-    exit; // Exit if accessed directly
+  exit; // Exit if accessed directly
 }
 
 class ss_branding_widget extends WP_Widget {
 	
- // Sets the widgets slug
- protected $widget_slug = 'branding_widget';
+  // Sets the widgets slug
+  protected $widget_slug = 'branding_widget';
 
- public function __construct() {
-  parent::__construct(
-   $this->get_widget_slug(),
-   __( 'Shoestrap: Branding', $this->get_widget_slug() ),
-   array(
-    'classname'   => $this->get_widget_slug().'-class',
-    'description' => __( 'Displays the logo from Shoestrap.', $this->get_widget_slug() )
-   )
+  public function __construct() {
+    parent::__construct(
+    $this->get_widget_slug(),
+    __( 'Shoestrap: Branding', $this->get_widget_slug() ),
+    array(
+      'classname'   => $this->get_widget_slug().'-class',
+      'description' => __( 'Displays the logo from Shoestrap.', $this->get_widget_slug() )
+    )
   );
 		
   // Refreshing the widget's cached output with each new post
@@ -39,104 +39,103 @@ class ss_branding_widget extends WP_Widget {
  }
 	
 	
- /**
-  * Return the widget slug.
-  */
- public function get_widget_slug() {
-  return $this->widget_slug;
- }
-
-
- /**
-  * Outputs the content of the widget.
-  *
-  */
- public function widget( $args, $instance ) {
-		
-  // Check if there is a cached output
-  $cache = wp_cache_get( $this->get_widget_slug(), 'widget' );
-
-  if ( !is_array( $cache ) ) {
-   $cache = array();
+  /**
+   * Return the widget slug.
+   */
+  public function get_widget_slug() {
+    return $this->widget_slug;
   }
 
-  if ( ! isset ( $args['widget_id'] ) ) {
-   $args['widget_id'] = $this->id;
+
+  /**
+   * Outputs the content of the widget.
+   *
+   */
+  public function widget( $args, $instance ) {
+		
+    // Check if there is a cached output
+    $cache = wp_cache_get( $this->get_widget_slug(), 'widget' );
+
+    if ( !is_array( $cache ) ) {
+      $cache = array();
+    }
+
+    if ( ! isset ( $args['widget_id'] ) ) {
+      $args['widget_id'] = $this->id;
+    }
+
+    if ( isset ( $cache[ $args['widget_id'] ] ) ) {
+      return print $cache[ $args['widget_id'] ];
+    }
+		
+    extract( $args, EXTR_SKIP );
+
+    $widget_string = $before_widget;
+		
+    // pull url from backend
+    if ( !empty( $instance['url'] ) ) {
+      $url = $instance['url'];
+    } else {
+      $url = home_url();
+    }
+		
+    ob_start();
+    include( plugin_dir_path( __FILE__ ) . 'views/widget.php' );
+    $widget_string .= ob_get_clean();
+    $widget_string .= $after_widget;
+
+    $cache[ $args['widget_id'] ] = $widget_string;
+
+    wp_cache_set( $this->get_widget_slug(), $cache, 'widget' );
+
+    print $widget_string;
+
+  } // end widget
+	
+	
+  public function flush_widget_cache() {
+    wp_cache_delete( $this->get_widget_slug(), 'widget' );
   }
+	
+	
+  /**
+   * Processes the widget's options to be saved.
+   *
+   */
+  public function update( $new_instance, $old_instance ) {
 
-  if ( isset ( $cache[ $args['widget_id'] ] ) ) {
-   return print $cache[ $args['widget_id'] ];
-  }
+    $instance = $old_instance;
 		
-  extract( $args, EXTR_SKIP );
+    $instance['enable_url'] = strip_tags( $new_instance['enable_url'] );
 
-  $widget_string = $before_widget;
+    return $instance;
+
+  } // end widget
+	
+	
+  /**
+    * Generates the administration form for the widget.
+    *
+    */
+  public function form( $instance ) {
+
+    $url = get_site_url();
 		
-  // pull urll from backend
-  if ( !empty( $instance['url'] ) ) {
-   $url = $instance['url'];
-  } else {
-   $url = home_url();
-  } 
-		
-  ob_start();
-  include( plugin_dir_path( __FILE__ ) . 'views/widget.php' );
-  $widget_string .= ob_get_clean();
-  $widget_string .= $after_widget;
+    //Set up some default widget settings.
+    $defaults = array( 
+      'title'       => 'URL:',
+      'url'         => '',
+      'placeholder' => $url,
+    );
 
-  $cache[ $args['widget_id'] ] = $widget_string;
+    $instance = wp_parse_args(
+      (array) $instance, $defaults
+    );
 
-  wp_cache_set( $this->get_widget_slug(), $cache, 'widget' );
+    // Display the admin form
+    include( plugin_dir_path(__FILE__) . 'views/admin.php' );
 
-  print $widget_string;
-
- } // end widget
-	
-	
- public function flush_widget_cache() 
- {
-  wp_cache_delete( $this->get_widget_slug(), 'widget' );
- }
-	
-	
- /**
-  * Processes the widget's options to be saved.
-  *
-  */
- public function update( $new_instance, $old_instance ) {
-
-  $instance = $old_instance;
-
-  $instance['url'] = strip_tags( $new_instance['url'] );
-
-  return $instance;
-
- } // end widget
-	
-	
- /**
-  * Generates the administration form for the widget.
-  *
-  */
- public function form( $instance ) {
-
-  $url = get_site_url();
-		
-  //Set up some default widget settings.
-  $defaults = array( 
-   'title'       => 'URL:',
-   'url'         => '',
-   'placeholder' => $url,
-  );
-
-  $instance = wp_parse_args(
-   (array) $instance, $defaults
-  );
-
-  // Display the admin form
-  include( plugin_dir_path(__FILE__) . 'views/admin.php' );
-
- } // end form
+  } // end form
 	
 	
 }
--- views/widget.php
+++ views/widget.php
@@ -1,7 +1,7 @@
 <div class="<?php echo ss_branding_widget::get_widget_slug();?>-wrapper <?php echo apply_filters( 'shoestrap_branding_wrapper_class', '' ); ?>">
- <div class="<?php echo apply_filters( 'shoestrap_branding_container_class', 'container' ); ?>">
-  <div class="<?php echo apply_filters( 'shoestrap_branding_column_class', 'col-sm-12' ); ?>">
-   <a class="<?php echo apply_filters( 'shoestrap_branding_link_class', 'brand-logo' ); ?>" href="<?php echo $url; ?>/"><?php echo Shoestrap_Branding::logo();?></a>
+  <div class="<?php echo apply_filters( 'shoestrap_branding_container_class', 'container' ); ?>">
+    <div class="<?php echo apply_filters( 'shoestrap_branding_column_class', 'col-sm-12' ); ?>">
+      <a class="<?php echo apply_filters( 'shoestrap_branding_link_class', 'brand-logo' ); ?>" href="<?php echo $url; ?>/"><?php echo Shoestrap_Branding::logo();?></a>
+    </div >
   </div >
- </div >
 </div >
